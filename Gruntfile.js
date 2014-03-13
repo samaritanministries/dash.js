c0afd0d6061cef7dff2e2bc7501f15393b4f7f1b
bwvoss <bwvoss@gmail.com>
2014-03-13 14:17:18 -0500

    Version 0.0.5

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,38 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
     uglify: {
-      my_target: {
+      options: {
+        mangle: true
+      },
+      dist: {
         files: {
-          'samaritan-js.min.js': ['scripts/bower_components/jquery/jquery.js',
-                                  'scripts/bower_components/uuid-js/lib/uuid.js',
-                                  'scripts/bower_components/cookies-js/src/cookies.js',
-                                  'scripts/smchcn_oauth/namespace.js',
-                                  'scripts/smchcn_oauth/config.js',
-                                  'scripts/smchcn_oauth/cookie.js',
-                                  'scripts/smchcn_oauth/browser_location.js',
-                                  'scripts/smchcn_oauth/params.js',
-                                  'scripts/smchcn_oauth/url_generator.js',
-                                  'scripts/smchcn_oauth/token_validator.js',
-                                  'scripts/smchcn_oauth/access_requester.js']
+          'dist/samaritan-js.min.js': [
+            'js/scripts/bower_components/jquery/jquery.js',
+            'js/scripts/bower_components/uuid-js/lib/uuid.js',
+            'js/scripts/bower_components/cookies-js/src/cookies.js',
+            'js/scripts/smchcn_oauth/namespace.js',
+            'js/scripts/smchcn_oauth/config.js',
+            'js/scripts/smchcn_oauth/cookie.js',
+            'js/scripts/smchcn_oauth/browser_location.js',
+            'js/scripts/smchcn_oauth/params.js',
+            'js/scripts/smchcn_oauth/url_generator.js',
+            'js/scripts/smchcn_oauth/token_validator.js',
+            'js/scripts/smchcn_oauth/access_requester.js'
+          ]
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'dist/framework.css' : 'css/framework.css/sass/framework.scss'
         }
       }
     }
   });
 
-  grunt.registerTask('build', [
-                     'uglify'
-  ]);
+  grunt.registerTask('build:js',   ['uglify']);
+  grunt.registerTask('build:sass', ['sass']);
+  grunt.registerTask('build', ['build:js', 'build:sass']);
 };
