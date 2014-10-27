3fd0b8a29bc101c9bcff6803717d2f6cb5183952
Taryn Sauer <taryn.sauer@gmail.com>
2014-10-27 14:31:55 -0500

    Version 1.0.5

--- js/spec/dash/browser.js
+++ js/spec/dash/browser.js
@@ -1,5 +1,15 @@
 describe("Dash.Browser", function() {
+
   it("returns the browser type as a string", function() {
     expect(Dash.Browser.Navigator.userAgent()).toEqual(jasmine.any(String));
   });
+
+  it("returns false when browser location does not include platform pathname", function() {
+    expect(Dash.Browser.Location.isIFramedBy('https://someurl.org/')).toBeFalsy();
+  });
+
+  it("returns true when browser location is in dev environment", function() {
+    expect(Dash.Browser.Location.isIFramedBy('https://localhost')).toBeTruthy();
+  });
+
 });
